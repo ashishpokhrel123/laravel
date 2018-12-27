d1f130b8d3116e056374bad17645641b0a63b58f
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2018-12-27 22:31:19 +0545

    design and auth

---
+++ Ems/resources/views/addstudent.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('content')
+<form>
+    <label>Name:</label>
+    <label>Address:</label>
+    <label>Email:</label>
+    <label>Dob:</label>
+    <label>Parent name:</label>
+
+</form>
+@endsection
